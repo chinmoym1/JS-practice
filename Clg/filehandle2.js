264f0f5255fa2a32771d3cc52444e5d36c7d31f1
chinmoym1 <majeechinmoy786@gmail.com>
2025-04-08 18:27:29 +0530

    change in FH2

--- Clg/filehandle2.js
+++ Clg/filehandle2.js
@@ -1,1 +1,20 @@
 // create any file in which data input by the user
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const fs = require('fs');
+rl.question('Please enter your name: ', (answer) => {
+  fs.appendFile('userInput.txt', `User  input: ${answer}\n`, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('User  input saved to file.');
+    }
+  });
+  rl.close();
+});
